2f7ccf1367ad5e0d03d64a41d2e0f3d555a82319
Eli <elimermz@gmail.com>
2026-06-18 16:35:54 +0300

    comeleted agent route

--- routes/agent_routes.py
+++ routes/agent_routes.py
@@ -6,6 +6,17 @@ agent = AgentDB()
 
 @router.post("/agents", status_code=201)
 def create_agent(data: dict):
+    if data["agent_rank"] not in ["Junior", "Senior", "Commander"]:
+        raise HTTPException(status_code=400, detail="rank not allowed")
+    if "name" not in data.keys():
+        raise HTTPException(status_code=422,detail="missing name")
+    if "agent_rank" not in data.keys():
+        raise HTTPException(status_code=422,detail="missing rank")
+    if "specialty" not in data.keys():
+        raise HTTPException(status_code=422,detail="specialty")
+    if not data:
+        raise HTTPException(status_code=422, detail="no data")
+
     agent.create_agent(data)
     return {"message": "agent created successfully"}
 
@@ -16,22 +27,28 @@ def get_all_agents():
 
 @router.get("/agents/{id}", status_code=200)
 def get_agent_by_id(id: int):
-
+    if not type(id) == int:
+        raise HTTPException(status_code=422, detail="not a number")
     if not agent.get_agent_by_id(id):
        raise HTTPException(status_code=404,detail="agent not found")
     return agent.get_agent_by_id(id)
 
 @router.put("/agents/{id}")
 def update_agent(id: int, data: dict):
+    if not type(id) == int:
+        raise HTTPException(status_code=422, detail="not a number")
     if not agent.update_agent(id, data):
         raise HTTPException(status_code=404, detail="Agent not found")
-    
+    if not data:
+        raise HTTPException(status_code=422, detail="no data")
     return {"message": "agent updated successfully"}
 
 
 @router.put("/agents/{id}/deactivate")
 def deactivate_agent(id: int):
     selected_agent = agent.get_agent_by_id(id)
+    if not type(id) == int:
+        raise HTTPException(status_code=422, detail="not a number")
     if selected_agent["is_active"] == False:
         raise HTTPException(status_code=400, detail="Agent is not active")
     if not selected_agent:
@@ -43,7 +60,9 @@ def deactivate_agent(id: int):
 
 @router.get("/agents/{id}/performance")
 def get_agent_performance(id: int):
-    return agent.get_agent_performance(id)
+    if agent.get_performance(id):
+        return agent.get_performance(id)
+    raise HTTPException(status_code=404, detail="agent not found")
 
 
 #check boolean sitch
